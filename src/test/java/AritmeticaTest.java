4bad655e3466cbca99371e79f440d476e28d4fdb
Marcos Javier Magni Mattoni <javie@MauricioP51.VirtusDomain.DM>
2019-01-29 17:18:02 +0100

    Probando los tests.

---
+++ src/test/java/AritmeticaTest.java
@@ -0,0 +1,25 @@
+import aritmetica.Aritmetica;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AritmeticaTest {
+    @Test
+    public void testSuma() {
+        //fail("Not yet implemented");
+        Aritmetica aritmetica = new Aritmetica();
+        assertEquals(2, aritmetica.suma(1,1), 0);
+    }
+    @Test
+    public void testResta() {
+        //fail("Not yet implemented");
+    }
+    @Test
+    public void testMultiplicacion() {
+        //fail("Not yet implemented");
+    }
+    @Test
+    public void testDivision() {
+        //fail("Not yet implemented");
+    }
+}
