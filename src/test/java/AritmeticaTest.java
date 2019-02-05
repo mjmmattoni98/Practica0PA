8e0715dbe57180db0dd42d6be3f067f3dd0ff6b0
Marcos Javier Magni Mattoni <javie@MauricioP51.VirtusDomain.DM>
2019-02-05 13:39:08 +0100

    Agregando ecuaciones.

--- src/test/java/AritmeticaTest.java
+++ src/test/java/AritmeticaTest.java
@@ -22,4 +22,14 @@ public class AritmeticaTest {
     public void testDivision() {
         //fail("Not yet implemented");
     }
+    @Test(expected = ArithmeticException.class)
+    public void divisionPorCero(){
+       // try {
+            System.out.println(1 / 0);
+         //   fail("Aqui no debe llegar");
+        //} catch (ArithmeticException e){
+
+        //}
+
+    }
 }
---
+++ src/test/java/ecuaciones/EcuacionLinealTest.java
@@ -0,0 +1,25 @@
+package ecuaciones;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+//import ecuacion.EcuacionLineal;
+
+public class EcuacionLinealTest {
+
+//	@Test
+//	public void terminoIndependienteCeroTest() {
+////		EcuacionLineal ecuacionLineal = new EcuacionLineal(1, 0); // x + 0 = 0
+//		// A partir de aquí, el código que comprueba que la solución es correcta
+//	}
+//
+//	@Test
+//	public void distintosDeCeroTest() {
+//		EcuacionLineal ecuacionLineal = new EcuacionLineal(1, 1); // x + 1 = 0
+//		// A partir de aquí, el código que comprueba que la solución es correcta
+//
+//		ecuacionLineal = new EcuacionLineal(2, 1); // 2x + 1 = 0
+//		// A partir de aquí, el código que comprueba que la solución es correcta
+//	}
+}
