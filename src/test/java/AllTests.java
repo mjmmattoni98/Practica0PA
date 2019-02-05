09f3d1487886ffc1a88ac4a069900f50ed605a05
Marcos Javier Magni Mattoni <javie@MauricioP51.VirtusDomain.DM>
2019-02-05 13:10:56 +0100

    Ejecutar mas de un test.

---
+++ src/test/java/AllTests.java
@@ -0,0 +1,8 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ AritmeticaTest.class, AritmeticaRestaParametrizadaTest.class })
+public class AllTests {
+
+}
